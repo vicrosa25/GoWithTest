516433ae95a5e04153bc7ca72fbf942ae7948827
Victor Rosa <vicrosa25@gmail.com>
2025-01-19 11:34:39 +0100

    Add word to dictionary finished

---
+++ class_07_maps/dictionary.go
@@ -0,0 +1,17 @@
+package class07maps
+
+import "errors"
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
---
+++ class_07_maps/dictionary_test.go
@@ -0,0 +1,52 @@
+package class07maps
+
+import (
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("Known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertString(t, got, want)
+	})
+
+	t.Run("Unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+		assertString(t, err.Error(), want)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+
+}
+
+func assertString(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertString(t, got, definition)
+}
